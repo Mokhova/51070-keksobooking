f89ed9fe8bc5457ed8211e245ce6cb9f727dab20
Kate Mokhova <kate.mokhova@kontur.ru>
2017-02-19 00:56:57 +0500

    #18 Полная изоляция

--- js/form.js
+++ js/form.js
@@ -1,29 +1,30 @@
 'use strict';
 
-var type = document.querySelector('#type');
-var timeIn = document.querySelector('#time');
-var timeOut = document.querySelector('#timeout');
-var capacity = document.querySelector('#capacity');
-var roomNumber = document.querySelector('#room_number');
-var priceTypeValues = ['1000', '0', '10000'];
-var timeValues = ['12', '13', '14'];
+(function () {
+  var type = document.querySelector('#type');
+  var timeIn = document.querySelector('#time');
+  var timeOut = document.querySelector('#timeout');
+  var capacity = document.querySelector('#capacity');
+  var roomNumber = document.querySelector('#room_number');
+  var priceTypeValues = ['1000', '0', '10000'];
+  var timeValues = ['12', '13', '14'];
 
 
-// Ограничения полей
-var formTitle = document.querySelector('#title');
-formTitle.required = true;
-formTitle.minLength = 30;
-formTitle.maxLength = 100;
+  // Ограничения полей
+  var formTitle = document.querySelector('#title');
+  formTitle.required = true;
+  formTitle.minLength = 30;
+  formTitle.maxLength = 100;
 
-var price = document.querySelector('#price');
-price.required = true;
-price.min = 1000;
-price.max = 10000;
+  var price = document.querySelector('#price');
+  price.required = true;
+  price.min = 1000;
+  price.max = 10000;
 
-document.querySelector('#address').required = true;
+  document.querySelector('#address').required = true;
 
-window.synchronizeFields(roomNumber, capacity, ['1', '2', '100'], ['0', '3', '3'], 'value');
-window.synchronizeFields(timeIn, timeOut, timeValues, timeValues, 'value');
-window.synchronizeFields(type, price, priceTypeValues, priceTypeValues, 'min');
+  window.synchronizeFields(roomNumber, capacity, ['1', '2', '100'], ['0', '3', '3'], 'value');
+  window.synchronizeFields(timeIn, timeOut, timeValues, timeValues, 'value');
+  window.synchronizeFields(type, price, priceTypeValues, priceTypeValues, 'min');
 
-window.initializePins();
+})();
